05360427e9a4215c88b293fc6e736015a99031ee
Stephen <stephenkago10@gmail.com>
2022-09-14 16:30:37 +0300

    add event

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ import './index.css'
 // formatting
 
 const books = [
-    {
+{   id: 1,
     img : 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/516XEz3XX4L._AC_SX184_.jpg',
     title: 'Where the Crawdads Sing',
     author: 'Delia Owens'
 },
 
-{
+{   id: 2,
     img : 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/51ezzFTCUEL._AC_SX184_.jpg',
     title: 'Holding the Line',
     author: 'Geoffrey Berman'
 },
 
-{
+{   id: 3,
     img : 'https://images-na.ssl-images-amazon.com/images/I/71j0FLAauxL._AC_UL210_SR195,210_.jpg',
     title: 'It Ends with Us',
     author: 'Colleen Hoover'
@@ -42,19 +42,25 @@ function BookList() {
         <section className='booklist'>         
             {books.map((book) => {
                 //const {img, title, author}= book;
-                return <Book book={book}></Book>;
+                return <Book key = {book.id} {...book}></Book>;
             })}
         </section>
     );
 }
 const Book = (props) => {
-    const { img, title, author, children } = props.book; //then you input just the parameters
+    const { img, title, author, children } = props; //then you input just the parameters
+    //attribute, eventHandler
+    //onClick, onouseOver
+    const clickHandler = () => (
+        alert('hello')
+    );
     return (
         <article className='book'>
             <img src={img} alt=''/>
             <h1>{title}</h1>
             <h2>{author}</h2>
-            <p>{children}</p>
+            {/* <p>{children}</p> */}
+            <button type='button' onClick={clickHandler}>Ref</button>
         </article>
     );
 };
